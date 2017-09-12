d2101db8f65ef6119d9aeabec582ce7fd321a619
Rachel Chervin <lanaganinc@gmail.com>
2017-09-12 08:37:19 -0700

    extract user photo method to model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,27 +1,32 @@
 class WelcomeController < ApplicationController
+
   def home
-    begin
-      if params[:screen_name].present?
+    if params[:screen_name].present?
+      begin
         @user = User.find_or_create_by!(screen_name: params[:screen_name].downcase)
         @tweets = @client.user_timeline(@user.screen_name).take(10)
 
-        if @user.last_tweet < @tweets.first.id
-          @tweets.each do |tweet|
-            next if tweet.id <= @user.last_tweet
-            Tweet.create!(content: tweet.text, user: @user)
+        if @tweets.empty?
+          flash[:notice] = :screen_name, "Sorry, this user has no tweets."
+        else
+          if @user.last_tweet < @tweets.first.id
+            @tweets.each do |tweet|
+              next if tweet.id <= @user.last_tweet
+              Tweet.create!(content: tweet.text, user: @user)
+            end
+            @user.update_attributes(last_tweet: @tweets.first.id)
           end
-          @user.update_attributes(last_tweet: @tweets.first.id)
         end
 
-        @user.update_attributes(profile_url: ("http://pbs.twimg.com" + @client.user(@user.screen_name).profile_image_url.path)) unless @user.profile_url
+        @user.update_photo @client.user(@user.screen_name).profile_image_url.path
       end
 
       true
-    rescue Twitter::Error::Unauthorized => e
+    rescue Twitter::Error => e
       flash[:notice] = :screen_name, "Sorry, that user's tweets aren't available right now."
 
       false
     end
-
   end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
 
   validates_presence_of :screen_name
   validates_uniqueness_of :screen_name
+
+  def update_photo image
+    update_attributes(profile_url: "http://pbs.twimg.com" + image)
+  end
 end
