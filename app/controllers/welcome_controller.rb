a5be7e9f761faa4c9849cb09a048541ab1144625
Rachel Chervin <lanaganinc@gmail.com>
2017-09-12 01:51:15 -0700

    install bulma CSS for some styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,7 @@ class WelcomeController < ApplicationController
     if params[:screen_name].present?
       @user = User.find_or_create_by!(screen_name: params[:screen_name].downcase)
       @tweets = @client.user_timeline(@user.screen_name).take(10)
+
       if @user.last_tweet < @tweets.first.id
         @tweets.each do |tweet|
           next if tweet.id <= @user.last_tweet
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
-  has_many :tweets
+  has_many :tweets, dependent: :destroy
 
   validates_presence_of :screen_name
   validates_uniqueness_of :screen_name
+
+  def self.get_tweets
+    @client.user_timeline(screen_name).take(10)
+  end
 end
