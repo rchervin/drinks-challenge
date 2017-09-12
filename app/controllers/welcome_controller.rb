7c2d7e75c07c1b05be8f090854a3670114aa332d
Rachel Chervin <lanaganinc@gmail.com>
2017-09-12 16:56:47 -0700

    add users index to show previous searches

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,10 @@
 class WelcomeController < ApplicationController
   before_action :clear_flash
-  before_action :set_screen_name
+  before_action :check_screen_name
 
   def home
     begin
-    @user = User.find_or_create_by!(screen_name: params[:screen_name].downcase)
+    @user = User.find_or_create_by!(screen_name: @screen_name)
     @tweets = @client.user_timeline(@user.screen_name).take(10)
 
     if @tweets.present?
@@ -15,7 +15,7 @@ class WelcomeController < ApplicationController
     end
 
     rescue Twitter::Error => e
-      flash[:notice] = :screen_name, "Unable to reach tweets for @#{@user.screen_name}."
+      flash[:notice] = :screen_name, "Unable to reach tweets for @#{@screen_name}."
     end
   end
 
@@ -25,9 +25,10 @@ class WelcomeController < ApplicationController
     flash[:notice] = ""
   end
 
-  def set_screen_name
+  def check_screen_name
     begin
       params[:screen_name] ||= "just_drinks"
+      @screen_name = params[:screen_name].downcase.gsub(/\s+/, "")
       @twitter_user = @client.user(@screen_name)
     rescue Twitter::Error => e
       flash[:notice] = :screen_name, "@#{@screen_name} does not seem to have a twitter account."
