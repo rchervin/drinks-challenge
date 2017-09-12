4cc2aad2432283580917d596fe603e339f085b73
Rachel Chervin <lanaganinc@gmail.com>
2017-09-12 00:26:00 -0700

    try of auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'twitter'
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
@@ -7,11 +9,24 @@ class ApplicationController < ActionController::Base
   private
 
   def set_twitter_client
-    @client = Twitter::REST::Client.new do |config|
-      config.consumer_key            = ENV["DC_TWITTER_CONSUMER_KEY"]
-      config.consumer_secret         = ENV["DC_TWITTER_SECRET_KEY"]
-      config.access_token            = ENV["DC_TWITTER_ACCESS_TOKEN"]
-      config.access_token_secret     = ENV["DC_TWITTER_ACCESS_TOKEN_SECRET"]
+    def client
+    Twitter.configure do |config|
+      config.consumer_key = TwitterToken.consumer.key
+      config.consumer_secret = TwitterToken.consumer.secret
+      config.oauth_token = token
+      config.oauth_token_secret = secret
     end
+    @client ||= Twitter::Client.new
+  end
+    # @client = Twitter::REST::Client.new do |config|
+    #   config.consumer_key            = ENV["DC_TWITTER_CONSUMER_KEY"]
+    #   config.consumer_secret         = ENV["DC_TWITTER_SECRET_KEY"]
+    #   config.access_token            = ENV["DC_TWITTER_ACCESS_TOKEN"]
+    #   config.access_token_secret     = ENV["DC_TWITTER_ACCESS_TOKEN_SECRET"]
+    # end
+    # string = "#{ENV['DC_TWITTER_CONSUMER_KEY']}:#{ENV['DC_TWITTER_SECRET_KEY']}"
+    # encoded_data = Base64.decode64(string)
+    consumer = OAuth::Consumer.new(ENV['DC_TWITTER_CONSUMER_KEY'], ENV['DC_TWITTER_SECRET_KEY'], :site => "http://api.twitter.com", :scheme => :header)
+    @access_token = OAuth::AccessToken.from_hash(consumer, :oauth_token => oauth_token, :oauth_token_secret => oauth_token_secret)
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,10 @@
 class WelcomeController < ApplicationController
   def home
-    if params[:screen_name]
-      @account = params[:screen_name].downcase
-      @user = User.find_or_create_by!(screen_name: @account)
-      @responses = @client.user_timeline(@user.screen_name).take(10)
+    if params[:screen_name].present?
+      @user = User.find_or_create_by!(screen_name: params[:screen_name].downcase)
+      @tweets = @client.user_timeline(@user.screen_name).take(10)
+      @response = @access_token.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=chervillain&count=2")
+      logger.info @response.inspect
     end
   end
 end
