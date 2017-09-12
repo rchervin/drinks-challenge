20d6bc989b91dd4a3ede900d8819ab7c1a1cf564
Rachel Chervin <lanaganinc@gmail.com>
2017-09-11 18:43:36 -0700

    configure twitter authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_twitter_client
+
+  private
+
+  def set_twitter_client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["DC_TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["DC_TWITTER_SECRET_KEY"]
+    end
+  end
 end
